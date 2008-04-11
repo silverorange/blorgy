8bfb95e6cbbe608b6d8d74c759a2a222e4df1f4d
Michael Gauthier <mike@silverorange.com>
2008-04-11 17:37:26 +0000

    Fix text selection IE6 bug for Blorgy. This may also fix #2152. Refs #2152.
    svn commit r30385

--- include/layouts/xhtml/common/htmlhead.php
+++ include/layouts/xhtml/common/htmlhead.php
@@ -1,6 +1,6 @@
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-	<base href="<?= $this->basehref ?>" />
+	<base href="<?= $this->basehref ?>"></base>
 	<?=$this->html_head_entries?>
 	<title><?= $this->html_title ?></title>
 	<link rel="icon" href="favicon.ico" type="image/x-icon" />
