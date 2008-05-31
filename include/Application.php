bdd7a9a73711c172f7bdc6a0af42c86642e34099
Michael Gauthier <mike@silverorange.com>
2008-05-31 04:18:40 +0000

    Support custom layouts and templates as per the specification.
    svn commit r33039

--- include/Application.php
+++ include/Application.php
@@ -107,14 +107,60 @@ class Application extends SiteWebApplication
 			break;
 
 		default:
-			require_once '../include/layouts/BlorgyLayout.php';
-			$layout = new BlorgyLayout($this,
-				'../include/layouts/xhtml/default.php');
-
+			$layout = $this->resolveThemeLayout();
 			break;
 		}
 
 		return $layout;
+	}
+
+	// }}}
+	// {{{ protected function resolveThemeLayout()
+
+	protected function resolveThemeLayout()
+	{
+		require_once '../include/layouts/BlorgyLayout.php';
+
+		$theme = $this->config->site->theme;
+
+		$layout_class = 'BlorgyLayout';
+
+		// get camel-case version of theme shortname
+		$class_name = str_replace(' ', '',
+			ucwords(str_replace('_', ' ', $theme))).'Layout';
+
+		// check if theme layout exists
+		$theme_layout_file = dirname(__FILE__).'/../themes/'.$theme.
+			'/layouts/'.$class_name.'.php';
+
+		if (file_exists($theme_layout_file)) {
+			require_once $theme_layout_file;
+			if (!class_exists($class_name)) {
+				throw new SiteException(sprintf('Layout file "%s" must '.
+					'contain a class named "%s"',
+					$theme_layout_file, $class_name));
+			}
+
+			if (!is_subclass_of($class_name, 'BlorgyLayout')) {
+				throw new SiteException(sprintf('Layout class "%s" must '.
+					'be a subclass of BlorgyLayout.',
+					$class_name));
+			}
+
+			$layout_class = $class_name;
+		}
+
+		$template_file = '../include/layouts/xhtml/default.php';
+
+		// check if theme template exists
+		$theme_template_file = dirname(__FILE__).'/../themes/'.$theme.
+			'/layouts/xhtml/template.php';
+
+		if (file_exists($theme_template_file)) {
+			$template_file = $theme_template_file;
+		}
+
+		return new $layout_class($this, $template_file);
 	}
 
 	// }}}
