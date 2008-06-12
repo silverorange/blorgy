1c9234b15a28e8a77712adbe45a35bbc57a34298
Charles Waddell <charles@silverorange.com>
2008-06-12 15:24:13 +0000

    Change the database field show to visible. Show is a reserved word in MySQL.
    svn commit r33515

--- sql/admin-changes.sql
+++ sql/admin-changes.sql
@@ -1,28 +1,28 @@
 -- section for creating and managing content
-INSERT INTO AdminSection (id, title, description, displayorder, show)
+INSERT INTO AdminSection (id, title, description, displayorder, visible)
 	VALUES (10, 'Content', null, 0, true);
 
-INSERT INTO AdminComponent (id, shortname, title, description, displayorder, section, enabled, show)
+INSERT INTO AdminComponent (id, shortname, title, description, displayorder, section, enabled, visible)
 	VALUES (11, 'Post',    'Posts',   NULL, 10, 10, true, true);
 
-INSERT INTO AdminComponent (id, shortname, title, description, displayorder, section, enabled, show)
+INSERT INTO AdminComponent (id, shortname, title, description, displayorder, section, enabled, visible)
 	VALUES (12, 'Tag',     'Tags',    NULL, 20, 10, true, true);
 
-INSERT INTO AdminSubComponent (id, component, shortname, title, show, displayorder)
+INSERT INTO AdminSubComponent (id, component, shortname, title, visible, displayorder)
 	VALUES (11, 11, 'Comments', 'Manage Comments', true, 10);
 
 -- section for adding and editing authors
-INSERT INTO AdminSection (id, title, description, displayorder, show)
+INSERT INTO AdminSection (id, title, description, displayorder, visible)
 	VALUES (20, 'Authors', null, 10, true);
 
-INSERT INTO AdminComponent (id, shortname, title, description, displayorder, section, enabled, show)
+INSERT INTO AdminComponent (id, shortname, title, description, displayorder, section, enabled, visible)
 	VALUES (13, 'Author',  'Authors', NULL, 10, 20, true, true);
 
 -- section for sidebar, site title, theme, etc
-INSERT INTO AdminSection (id, title, description, displayorder, show)
+INSERT INTO AdminSection (id, title, description, displayorder, visible)
 	VALUES (30, 'Configuration', null, 20, true);
 
-INSERT INTO AdminComponent (id, shortname, title, description, displayorder, section, enabled, show)
+INSERT INTO AdminComponent (id, shortname, title, description, displayorder, section, enabled, visible)
 	VALUES (14, 'Sidebar', 'Sidebar', NULL, 10, 30, true, true);
 
 
