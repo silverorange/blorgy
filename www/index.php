8350493b2820d602b673002c8af5dec73df47032
Nathan Fredrickson <nathan@silverorange.com>
2008-06-13 14:40:49 +0000

    Copy to live branch.
    svn commit r33581

---
+++ www/index.php
@@ -0,0 +1,18 @@
+<?php
+
+@include_once 'PackageConfig.php';
+if (class_exists('PackageConfig')) {
+	PackageConfig::addPackage('swat');
+	PackageConfig::addPackage('site');
+	PackageConfig::addPackage('admin');
+	PackageConfig::addPackage('blorg');
+	PackageConfig::addPackage('nate-go-search');
+}
+
+require_once '../include/Application.php';
+
+$config_filename = dirname(__FILE__).'/../blorgy.ini';
+$app = new Application('blorgy', $config_filename);
+$app->run();
+
+?>
