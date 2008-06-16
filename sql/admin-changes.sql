b01ef838bb5424bc9305c7e953c6b5c4c944c5d5
Michael Gauthier <mike@silverorange.com>
2008-06-16 04:33:37 +0000

    Add article component to blorgy admin changes
    svn commit r33689

--- sql/admin-changes.sql
+++ sql/admin-changes.sql
@@ -3,10 +3,13 @@ INSERT INTO AdminSection (id, title, description, displayorder, visible)
 	VALUES (10, 'Content', null, 0, true);
 
 INSERT INTO AdminComponent (id, shortname, title, description, displayorder, section, enabled, visible)
-	VALUES (11, 'Post', 'Posts',   null, 10, 10, true, true);
+	VALUES (11, 'Post', 'Posts', null, 10, 10, true, true);
 
 INSERT INTO AdminComponent (id, shortname, title, description, displayorder, section, enabled, visible)
-	VALUES (12, 'Tag', 'Tags',    null, 20, 10, true, true);
+	VALUES (12, 'Tag', 'Tags', null, 20, 10, true, true);
+
+INSERT INTO AdminComponent (id, shortname, title, description, displayorder, section, enabled, visible)
+	VALUES (13, 'Article', 'Articles', null, 30, 10, true, true);
 
 INSERT INTO AdminSubComponent (id, component, shortname, title, visible, displayorder)
 	VALUES (11, 11, 'Comments', 'Manage Comments', true, 10);
@@ -16,17 +19,17 @@ INSERT INTO AdminSection (id, title, description, displayorder, visible)
 	VALUES (20, 'Authors', null, 10, true);
 
 INSERT INTO AdminComponent (id, shortname, title, description, displayorder, section, enabled, visible)
-	VALUES (13, 'Author', 'Authors', null, 10, 20, true, true);
+	VALUES (14, 'Author', 'Authors', null, 10, 20, true, true);
 
 -- section for sidebar, site title, theme, etc
 INSERT INTO AdminSection (id, title, description, displayorder, visible)
 	VALUES (30, 'Configuration', null, 20, true);
 
 INSERT INTO AdminComponent (id, shortname, title, description, displayorder, section, enabled, visible)
-	VALUES (14, 'Sidebar', 'Sidebar', null, 10, 30, true, true);
+	VALUES (15, 'Sidebar', 'Sidebar', null, 10, 30, true, true);
 
 INSERT INTO AdminComponent (id, shortname, title, description, displayorder, section, enabled, visible)
-	VALUES (15, 'Select Theme', 'Theme', null, 20, 30, true, true);
+	VALUES (16, 'Select Theme', 'Theme', null, 20, 30, true, true);
 
 
 -- update sequences
@@ -38,4 +41,4 @@ SELECT setval('adminsubcomponent_id_seq', max(id)) FROM AdminSubComponent;
 -- add bindings to default groups
 INSERT INTO AdminComponentAdminGroupBinding (component, groupnum)
 	SELECT AdminComponent.id, AdminGroup.id FROM AdminComponent, AdminGroup
-	WHERE AdminComponent.id in (11, 12, 13, 14, 15);
+	WHERE AdminComponent.id in (11, 12, 13, 14, 15, 16);
