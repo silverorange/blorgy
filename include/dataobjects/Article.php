fbefab2e19b66288bdb57c27f721bdf1074fabf9
Michael Gauthier <mike@silverorange.com>
2008-06-16 19:20:59 +0000

    Add optional instance to blorgy article laod method.
    svn commit r33755

--- include/dataobjects/Article.php
+++ include/dataobjects/Article.php
@@ -18,6 +18,54 @@ class Article extends SiteArticle
 	}
 
 	// }}}
+	// {{{ public function load()
+
+	/**
+	 * Loads this article 
+	 *
+	 * @param integer $id the database id of this article.
+	 * @param SiteInstance $instance optional. The instance to load the article
+	 *                                in. If the application does not use
+	 *                                instances, this should be null. If
+	 *                                upsecified, the instance is not checked.
+	 *
+	 * @return boolean true if this article was loaded and false if it was not.
+	 */
+	public function load($id, SiteInstance $instance = null)
+	{
+		$this->checkDB();
+
+		$loaded = false;
+		$row = null;
+		if ($this->table !== null && $this->id_field !== null) {
+			$id_field = new SwatDBField($this->id_field, 'integer');
+
+			$sql = sprintf('select * from %s where %s = %s',
+				$this->table,
+				$id_field->name,
+				$this->db->quote($id, $id_field->type));
+
+			$instance_id  = ($instance === null) ? null : $instance->id;
+			if ($instance_id !== null) {
+				$sql.=sprintf(' and instance %s %s',
+					SwatDB::equalityOperator($instance_id),
+					$this->db->quote($instance_id, 'integer'));
+			}
+
+			$rs = SwatDB::query($this->db, $sql, null);
+			$row = $rs->fetchRow(MDB2_FETCHMODE_ASSOC);
+		}
+
+		if ($row !== null) {
+			$this->initFromRow($row);
+			$this->generatePropertyHashes();
+			$loaded = true;
+		}
+
+		return $loaded;
+	}
+
+	// }}}
 }
 
 ?>
