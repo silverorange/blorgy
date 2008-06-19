bf7be33ec5e016faea78044ac477f74b1eade644
Nick Burka <nick@silverorange.com>
2008-06-19 16:18:31 +0000

    Handle really old .cfm links for relocating. This keeps old posts from suffering a bad case of link rot.
    svn commit r33871

--- include/pages/ExceptionPage.php
+++ include/pages/ExceptionPage.php
@@ -29,6 +29,8 @@ class ExceptionPage extends SiteExceptionPage
 			$this->relocateAuthor();
 		elseif ($source_exp[0] == 'rss')
 			$this->relocateFeed();
+		elseif (substr($_GET['source'], 0, 9) == 'index.cfm')
+			$this->relocateReallyOldPage();
 	}
 
 	// }}}
@@ -98,6 +100,53 @@ class ExceptionPage extends SiteExceptionPage
 		elseif ($source[0] == 'categories' && count($source) == 2) {
 			$this->app->relocate('tag/'.$source[1].'/feed');
 		}
+	}
+
+	// }}}
+	// {{{ private function relocateReallyOldPage()
+
+	private function relocateReallyOldPage()
+	{
+		if (isset($_GET['article']))
+			$this->relocateReallyOldPost($_GET['article']);
+		elseif (isset($_GET['name']))
+			$this->relocateReallyOldAuthor($_GET['name']);
+	}
+
+	// }}}
+	// {{{ private function relocateReallyOldPost()
+
+	private function relocateReallyOldPost($id)
+	{
+		$post = new BlorgPost();
+		$post->setDatabase($this->app->db);
+		$post->load($id, $this->app->getInstance());
+
+		if ($post->id === null)
+			return;
+
+		$path = $this->app->config->blorg->path.'archive';
+
+		$date = clone $post->publish_date;
+		$date->convertTZ($this->app->default_time_zone);
+		$year = $date->getYear();
+		$month_name = BlorgPageFactory::$month_names[$date->getMonth()];
+
+		$url = sprintf('%s/%s/%s/%s',
+			$path,
+			$year,
+			$month_name,
+			$post->shortname);
+
+		$this->app->relocate($url);
+	}
+
+	// }}}
+	// {{{ private function relocateReallyOldAuthor()
+
+	private function relocateReallyOldAuthor($name)
+	{
+		$this->app->relocate('author/'.$name);
 	}
 
 	// }}}
