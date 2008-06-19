aae8ab8c7d52918ba957825a3d9706e97d3d30f7
Nathan Fredrickson <nathan@silverorange.com>
2008-06-19 20:34:23 +0000

    Update persistent data.
    svn commit r33924

---
+++ sql/persistent-data/configsetting.sql
@@ -0,0 +1,26 @@
+--
+-- PostgreSQL database dump
+--
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = off;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET escape_string_warning = off;
+
+SET search_path = public, pg_catalog;
+
+--
+-- Data for Name: configsetting; Type: TABLE DATA; Schema: public; Owner: php
+--
+
+ALTER TABLE configsetting DISABLE TRIGGER ALL;
+
+
+
+ALTER TABLE configsetting ENABLE TRIGGER ALL;
+
+--
+-- PostgreSQL database dump complete
+--
+
