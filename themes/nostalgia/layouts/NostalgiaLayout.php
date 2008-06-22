6c00974ee806e05e64e9c19f82b17bacddd0a11b
Steven Garrity <steven@silverorange.com>
2008-06-22 16:45:24 +0000

    Add a new Nostalgia theme based on the beauty of the web in 1993
    svn commit r34079

---
+++ themes/nostalgia/layouts/NostalgiaLayout.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once '../include/layouts/BlorgyLayout.php';
+
+/**
+ * Layout for the Nostalgia theme
+ *
+ * @package   Blörgy
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class NostalgiaLayout extends BlorgyLayout
+{
+	// {{{ protected function displayHeaderTitle()
+
+	protected function displayHeaderTitle()
+	{
+		parent::displayHeaderTitle();
+		echo '<div><img src="themes/nostalgia/images/rainbow.png" alt="Rainbow!" height="5" width="100%" /></div>';
+	}
+
+	// }}}
+	// {{{ protected function finalizeBaseCss()
+
+	protected function finalizeBaseCss()
+	{
+	}
+
+	// }}}
+}
+
+?>
