e445456155263c33edbf21f8ef3f93e7ef678437
Michael Gauthier <mike@silverorange.com>
2008-06-29 04:33:44 +0000

    Remove hard-coded Google Analytics code from FOL theme. Blorg supports this. You can set your GA id in the config editor in the admin.
    svn commit r34423

--- themes/fol/layouts/xhtml/template.php
+++ themes/fol/layouts/xhtml/template.php
@@ -42,15 +42,7 @@
 	<?= $this->sidebar ?>
 </div>
 
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-var pageTracker = _gat._getTracker("UA-3504450-3");
-pageTracker._initData();
-pageTracker._trackPageview();
-</script>
+<?= $this->google_analytics ?>
 
 </body>
 </html>
