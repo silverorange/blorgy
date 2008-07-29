e537c7815576554269ef22144d4cf211752d33f2
Michael Gauthier <mike@silverorange.com>
2008-07-29 18:47:26 +0000

    Add more test cases.
    svn commit r35485

--- tests/cases/SearchTestCase.php
+++ tests/cases/SearchTestCase.php
@@ -45,11 +45,41 @@ class SearchTestCase extends TestCase
 	// }}}
 	// {{{ public function testSpellCheck()
 
-	public function testNoResults()
+	public function testSpellCheckResults()
 	{
 		$this->selenium->open('search?keywords=delicius');
 		$this->assertTrue($this->selenium->isTextPresent(
 			'Did you mean'));
+	}
+
+	// }}}
+	// {{{ public function testPostPagination()
+
+	public function testPostPagination()
+	{
+		$this->selenium->open('search?keywords=test&type=post&page=2');
+		$this->assertTrue($this->selenium->isTextPresent('Page 2'));
+		$this->assertTrue($this->selenium->isElementPresent(
+			"xpath=//div[contains(@class, 'entry')]/".
+			"div[contains(@class, 'entry-content')]"));
+
+		$this->assertFalse($this->selenium->isTextPresent(
+			'Article Search Results'));
+	}
+
+	// }}}
+	// {{{ public function testArticlePagination()
+
+	public function testArticlePagination()
+	{
+		$this->selenium->open('search?keywords=test&type=article&page=2');
+		$this->assertTrue($this->selenium->isTextPresent('Page 2'));
+		$this->assertTrue($this->selenium->isTextPresent(
+			'Article Search Results'));
+
+		$this->assertFalse($this->selenium->isElementPresent(
+			"xpath=//div[contains(@class, 'entry')]/".
+			"div[contains(@class, 'entry-content')]"));
 	}
 
 	// }}}
