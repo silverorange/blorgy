5f08d7829b21bf7a62449102be26d47e2bf3d5ad
Nick Burka <nick@silverorange.com>
2008-10-26 15:03:02 +0000

    A new layout for a blog. Still needs a little IE work.
    svn commit r38141

---
+++ themes/babies/layouts/BabiesLayout.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once '../include/layouts/BlorgyLayout.php';
+
+/**
+ * Layout for the Babies theme
+ *
+ * @package   Blörgy
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class BabiesLayout extends BlorgyLayout
+{
+}
+
+?>
