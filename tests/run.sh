332f7cc6011c6b78c303ee8e16b784f801227803
Nathan Fredrickson <nathan@silverorange.com>
2009-06-08 15:37:42 +0000

    same changes to run script
    svn commit r46227

--- tests/run.sh
+++ tests/run.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 if [ "$1" == "" ]; then
-	phpunit --log-xml log.xml TestSuite
+	echo "phpunit --log-xml log.xml TestSuite"
+	/usr/bin/phpunit --log-xml log.xml TestSuite
 else
 	file="$1"
 	class="`basename "$file" .php`"
-	phpunit --verbose $class "$file"
+	echo "phpunit $class $file"
+	/usr/bin/phpunit $class "$file"
 fi
