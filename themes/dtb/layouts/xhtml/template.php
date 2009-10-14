5a9f87c89f1af3b0f313ee44bf7b25a52c8db249
Daniel Burka <dburka@silverorange.com>
2009-10-14 18:38:08 +0000

    A ton of wide reaching changes fr the new version of my blog.
    svn commit r50510

---
+++ themes/dtb/layouts/xhtml/template.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<base href="<?= $this->basehref ?>"></base>
+    
+    <!--[if gte IE 7]><!-->
+        <?=$this->html_head_entries?>
+	<!--<![endif]-->
+	
+    <!--[if lte IE 6]>
+        <link rel="stylesheet" type="text/css" href="<?= $this->basehref ?>themes/dtb/styles/ie.css" media="all" />
+    <![endif]-->	      
+
+	<script type="text/javascript" src="<?= $this->basehref ?>themes/dtb/javascript/last.fm.records.js"></script>
+	<script type="text/javascript" src="<?= $this->basehref ?>themes/dtb/javascript/jquery-1.3.2.min.js"></script>
+	<title><?= $this->html_title ?></title>
+	<link rel="icon" href="favicon.ico" type="image/x-icon" />
+	<meta name="description" content="<?= $this->meta_description ?>" />
+</head>
+
+<body>
+
+    <div class="contents"></div>
+
+	<div class="container"><div class="inner-container">
+
+    	<div class="main">
+
+            <div id="header">
+            <h1><a href="<?= $this->basehref ?>">Delta Tango Bravo</a></h1>
+            <em>Alpha Whiskey Echo Bravo Lima Oscar Golf</em>
+            </div>
+
+            <?= $this->navbar ?>
+
+            <?= $this->title ?>
+            <?= $this->content ?>
+
+        </div>
+
+        <div class="extra">
+			<?= $this->sidebar ?>
+        </div>
+
+    	<br style="clear: both;" />
+
+	</div></div>
+
+
+    <div class="contents-footer"></div>
+
+    <div class="container container-footer"><div class="inner-container">
+
+        <div class="main">
+            <h3><span>Summary</span></h3>
+            <p>Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, <a href="other.html">sed do eiusmod</a> tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h2>Contact</h2>
+            <p>I'm not currently looking for any kind of work and sometimes I take a long time to respond to email, but if you'd like to contact me please send an email to daniel [at] deltatangobravo [dot] com</p>
+            <h2>Elsewhere</h2>
+            <p style="padding-top: 1em;">I tweet when the mood strikes at <a href="http://twitter.com/durka">Twitter</a></p>
+            <p>I sometimes review products on <a href="http://stuff.silverorange.com">Silverorange Stuff</a></p>
+            <p>I sell my amateur photography on <a href="http://clustershot.com/durka">Clustershot</a></p>
+            <p>I post some of my personal photos on <a href="http://flickr.com/durka">Flickr</a></p>
+            <p>I used to contribute on the <a href="http://blog.digg.com/?author=18">Digg Weblog</a></p>
+            <h2>This Blog</h2>
+            <p>The technology that powers this blog was built by <a href="http://silverorange.com/">silverorange</a> on their <a href="https://code.silverorange.com/wiki/Swat">Swat</a> open-source web application toolkit. I'm no longer working on day-to-day projects at silverorange, but they're available for hire if you're looking for top-notch interface design or web application development.</p>
+        </div>
+
+        <div class="extra">
+
+            <h3 class="title-speaking">Speaking</h3>
+            <ul class="list">
+                <li><em>Feb 22-23</em> <a href="http://2010.incontrolconference.com/">Orlando / In Control</a></li>
+                <li><em>Nov 16-17, 2010</em> <a href="http://events.carsonified.com/fowd/2009/nyc">NYC / Future of Web Design</a></li>
+                <li><em>Feb 15-19, 2010</em> <a href="http://www.webstock.org.nz/talks/events/">Wellington / Webstock</a></li>
+                <li><em>Tentative May 2010</em> <a href="http://www.aimconference.com/">Halifax / AIM</a></li>
+            </ul>
+
+            <h3 class="title-spoke">Spoke</h3>
+            <ul class="list">
+                <li><em>Jun 11, 2008</em> <a href="http://2009.incontrolconference.com/">Cincinnati / In Control</a></li>
+                <li><em>May 2010</em> <a href="http://www.aimconference.com/Daniel-Burka-Digg.com-Silver-Orange.html">Halifax / AIM</a></li>
+                <li><em>Mar 12-16, 2008</em> <a href="http://sxsw.com/">Austin / SXSW Interactive</a></li>
+                <li><em>Feb 20, 2009</em> <a href="http://www.slideshare.net/dburka/fowa-miami-2009-workshop">Miami / Future of Web Apps</a></li>
+                <li><em>Nov 4, 2008</em> <a href="http://events.carsonified.com/fowd/">NYC / Future of Web Design</a></li>
+                <li><em>Sep 27, 2008</em> <a href="http://www.webdirections.org/resources/daniel-burka-interaction-design-case-studies/">Sydney / Web Directions South</a></li>
+                <li><em>Sep 4, 2008</em> <a href="http://2008.dconstruct.org/schedule/DanielBurka.php">Brighton / dConstruct</a></li>
+                <li><em>Sep 24, 2008</em> <a href="http://www.webdesign-festival.com/2008/">Limoges / WiF</a></li>
+                <li><em>May 15, 2008</em> <a href="http://www.meshconference.com/">Toronto / MESH</a></li>
+                <li><em>Apr 23, 2008</em> <a href="http://events.carsonified.com/fowd/">London / Future of Web Design</a></li>
+                <li><em>Mar 20, 2008</em> <a href="http://sxsw.com/">Austin / SXSW Interactive</a></li>
+                <li><em>Jan 29, 2008</em> <a href="http://north08.webdirections.org/">Vancouver / Web Directions North</a></li>
+                <li><em>Oct 12, 2007</em> <a href="http://events.carsonified.com/fowa/">London / Future of Web Apps</a></li>
+            </ul>
+
+        </div>
+        <br style="clear: both;" />
+    </div></div>
+
+<?= $this->google_analytics ?>
+
+</body>
+</html>
