f321256ed19b6351475623e8cad61c2a3b30b389
Steven Garrity <steven@silverorange.com>
2009-10-22 16:29:17 +0000

    Copy a theme to start a Travel theme for Dan to work on
    svn commit r50744

---
+++ themes/travel/views/NostalgiaPostView.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'Blorg/views/BlorgPostView.php';
+
+/**
+ * Custom post view for the Nostalgia theme
+ *
+ * @package   Blörgy
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class NostalgiaPostView extends BlorgPostView
+{
+	// general display methods
+	// {{{ protected function displayFooter()
+
+	public function display($post)
+	{
+		parent::display($post);
+		echo '<hr class="entry-footer-rule" />';
+	}
+
+	// }}}
+}?>
