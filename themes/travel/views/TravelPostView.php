922f067be70cfdb7e1195dbbebbc42769d1b1b81
Steven Garrity <steven@silverorange.com>
2009-10-22 16:31:36 +0000

    Basic setup for Dan's Travel theme
    svn commit r50745

--- themes/travel/views/TravelPostView.php
+++ themes/travel/views/TravelPostView.php
@@ -3,13 +3,13 @@
 require_once 'Blorg/views/BlorgPostView.php';
 
 /**
- * Custom post view for the Nostalgia theme
+ * Custom post view for the Travel theme
  *
  * @package   Blörgy
  * @copyright 2008 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-class NostalgiaPostView extends BlorgPostView
+class TravelPostView extends BlorgPostView
 {
 	// general display methods
 	// {{{ protected function displayFooter()
@@ -17,7 +17,6 @@ class NostalgiaPostView extends BlorgPostView
 	public function display($post)
 	{
 		parent::display($post);
-		echo '<hr class="entry-footer-rule" />';
 	}
 
 	// }}}
