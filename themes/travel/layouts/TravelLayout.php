60e2daaf10827bbc4ea676bf343ca9a95a3336b6
Keith Burgoyne <keith@silverorange.com>
2009-11-03 14:35:58 +0000

    sync to ruby
    svn commit r50990

---
+++ themes/travel/layouts/TravelLayout.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once '../include/layouts/BlorgyLayout.php';
+
+/**
+ * Layout for the Travel theme
+ *
+ * @package   Blörgy
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class TravelLayout extends BlorgyLayout
+{
+	// {{{ protected function finalizeBaseCss()
+
+	protected function finalizeBaseCss()
+	{
+	}
+
+	// }}}
+}
+
+?>
---
+++ themes/travel/views/TravelPostView.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once 'Blorg/views/BlorgPostView.php';
+
+/**
+ * Custom post view for the Travel theme
+ *
+ * @package   Blörgy
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class TravelPostView extends BlorgPostView
+{
+	// general display methods
+	// {{{ protected function displayFooter()
+
+	public function display($post)
+	{
+		parent::display($post);
+	}
+
+	// }}}
+}?>
