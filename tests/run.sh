aac3fae24cbc9019890ba060257e4279fed1cf78
Michael Gauthier <mike@silverorange.com>
2010-06-01 18:38:45 +0000

    Fix deprecated --log-xml flag use.
    svn commit r55589

--- tests/run.sh
+++ tests/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ "$1" == "" ]; then
-	echo "phpunit --log-xml log.xml TestSuite"
-	/usr/bin/phpunit --log-xml log.xml TestSuite
+	echo "phpunit --log-junit log.xml TestSuite"
+	/usr/bin/phpunit --log-junit log.xml TestSuite
 else
 	file="$1"
 	class="`basename "$file" .php`"
