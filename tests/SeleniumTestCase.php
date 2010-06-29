e97103d66f74362ae21cb87f930d1c09142dc773
Keith Burgoyne <keith@silverorange.com>
2010-06-29 19:10:12 +0000

    moved to custom ff profile
    svn commit r56121

--- tests/SeleniumTestCase.php
+++ tests/SeleniumTestCase.php
@@ -12,7 +12,7 @@ class SeleniumTestCase extends TestCase
 		parent::setUp();
 
 		$this->selenium = new Testing_Selenium(
-			'*chrome /usr/local/bin/firefox-bin',
+			'*custom /usr/local/bin/firefox-bin',
 			$this->base_href
 		);
 
