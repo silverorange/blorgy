742c28ff5c643b20ad1dd1050182cbd88c8980f8
Michael Gauthier <mike@silverorange.com>
2010-10-12 19:14:48 +0000

    Better test boot-strap for Blorgy.
    svn commit r57959

--- tests/run.sh
+++ tests/run.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 if [ "$1" == "" ]; then
 	echo "phpunit --log-junit log.xml --verbose TestSuite"
-	/usr/bin/phpunit --log-junit log.xml TestSuite
+	/usr/bin/phpunit \
+		--include-path '/so/packages/hot-date/trunk:/so/packages/swat/trunk' \
+		--log-junit log.xml \
+		--verbose \
+		TestSuite
 else
 	file="$1"
 	class="`basename "$file" .php`"
 	echo "phpunit --verbose $class $file"
-	/usr/bin/phpunit $class "$file"
+	/usr/bin/phpunit \
+		--include-path '/so/packages/hot-date/trunk:/so/packages/swat/trunk' \
+		--verbose \
+		$class "$file"
 fi
