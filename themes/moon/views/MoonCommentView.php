15407d78a882d98a4388230f683b8287713f1e4c
Michael Gauthier <mike@silverorange.com>
2010-10-20 17:04:12 +0000

    Blorgy theme updates for HotDate.
    svn commit r58175

--- themes/moon/views/MoonCommentView.php
+++ themes/moon/views/MoonCommentView.php
@@ -54,13 +54,14 @@ class MoonCommentView extends BlorgCommentView
 			// display machine-readable date in UTC
 			$abbr_tag = new SwatHtmlTag('abbr');
 			$abbr_tag->class = 'comment-published';
-			$abbr_tag->title =
-				$comment->createdate->getDate(DATE_FORMAT_ISO_EXTENDED);
+			$abbr_tag->title = $comment->createdate->getISO8601();
 
 			// display human-readable date in local time
 			$date = clone $comment->createdate;
 			$date->convertTZ($this->app->default_time_zone);
-			$abbr_tag->setContent($date->format('%i:%M %p | %e %B %Y'));
+			$abbr_tag->setContent(
+				$date->formatLikeIntl('h:mm a \'|\' d MMMM yyyy'));
+
 			$abbr_tag->display();
 
 			$permalink_tag->close();
--- themes/moon/views/MoonPostView.php
+++ themes/moon/views/MoonPostView.php
@@ -107,13 +107,12 @@ class MoonPostView extends BlorgPostView
 			// display machine-readable date in UTC
 			$abbr_tag = new SwatHtmlTag('abbr');
 			$abbr_tag->class = 'published';
-			$abbr_tag->title =
-				$post->publish_date->getDate(DATE_FORMAT_ISO_EXTENDED);
+			$abbr_tag->title = $post->publish_date->getISO8601();
 
 			// display human-readable date in local time
 			$date = clone $post->publish_date;
 			$date->convertTZ($this->app->default_time_zone);
-			$abbr_tag->setContent($date->format('%e %B %Y'));
+			$abbr_tag->setContent($date->formatLikeIntl('d MMMM yyyy'));
 			$abbr_tag->display();
 
 			$permalink_tag->close();
