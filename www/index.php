0f3859a946ec2b10061d32ae55272b661d8e4635
Nick Burka <nick@silverorange.com>
2010-10-21 14:51:21 +0000

    merge live
    svn commit r58241

--- www/index.php
+++ www/index.php
@@ -12,7 +12,19 @@ if (class_exists('PackageConfig')) {
 	PackageConfig::addPackage('concentrate');
 }
 
+function shutdownDebug() {
+	$usage = getrusage();
+	if ($usage["ru_utime.tv_sec"] >= 29) {
+		error_log('Timeout! '.$usage["ru_utime.tv_sec"]."s - ".
+			$_SERVER['SCRIPT_URI']." - ".
+			$_SERVER['HTTP_USER_AGENT']." - ".
+			date("F j, Y, g:i a"));
+	}
+}
+register_shutdown_function('shutdownDebug');
+
 require_once '../include/Application.php';
+
 
 $config_filename = dirname(__FILE__).'/../blorgy.ini';
 $app = new Application('blorgy', $config_filename);
