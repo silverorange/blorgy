98476339f9df87770d72fbf4528aedb2bebe2793
Michael Gauthier <mike@silverorange.com>
2011-09-07 01:42:17 +0000

    New Blorg theme.
    svn commit r65014

---
+++ themes/mooncakes/layouts/xhtml/template.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<base href="<?= $this->basehref ?>"></base>
+	<?= $this->html_head_entries ?>
+	<title><?= $this->html_title ?></title>
+	<meta name="description" content="<?= $this->meta_description ?>" />
+	<?= $this->analytics ?>
+
+	<link href="http://fonts.googleapis.com/css?family=Delius" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+<div id="doc">
+
+	<div id="hd" class="clearfix">
+		<?= $this->header_title ?>
+		<?= $this->tagline ?>
+	</div>
+
+	<div id="bd">
+
+		<div id="content">
+			<?= $this->navbar ?>
+			<?= $this->title ?>
+			<?= $this->content ?>
+		</div>
+
+		<div id="sidebar">
+			<?= $this->sidebar ?>
+		</div>
+
+		<div class="clearfix"></div>
+
+	</div>
+
+	<div id="ft">
+	</div>
+
+</div>
+
+</body>
+</html>
---
+++ themes/mooncakes/views/MooncakesPostView.php
@@ -0,0 +1,123 @@
+<?php
+
+require_once 'Blorg/views/BlorgPostView.php';
+
+/**
+ * @package   Blörgy
+ * @copyright 2011 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class MooncakesPostView extends BlorgPostView
+{
+	// {{{ public function __construct()
+
+	public function __construct(SiteApplication $app)
+	{
+		parent::__construct($app);
+		$this->setPartMode('author', SiteView::MODE_NONE);
+	}
+
+	// }}}
+
+	// general display methods
+	// {{{ protected function displaySubHeader()
+
+	/**
+	 * Displays the title and meta information for a weblog post
+	 *
+	 * @param BlorgPost $post
+	 */
+	protected function displaySubHeader(BlorgPost $post)
+	{
+		ob_start();
+		$this->displayAuthor($post);
+		$author = ob_get_clean();
+
+		ob_start();
+		$this->displayPermalink($post);
+		$permalink = ob_get_clean();
+
+		ob_start();
+		$this->displayCommentCount($post);
+		$comment_count = ob_get_clean();
+
+		echo '<div class="entry-subtitle">';
+
+		/*
+		 * Comment count is shown if and only if comment_count element is shown
+		 * AND the following:
+		 * - comments are locked AND there is one or more visible comment OR
+		 * - comments are open OR
+		 * - comments are moderated.
+		 */
+		$show_comment_count =
+			(strlen($comment_count) > 0 &&
+				(($post->comment_status == SiteCommentStatus::LOCKED &&
+					count($post->getVisibleComments()) > 0) ||
+				$post->comment_status == SiteCommentStatus::OPEN ||
+				$post->comment_status == SiteCommentStatus::MODERATED));
+
+		if (strlen($author) > 0) {
+			if ($show_comment_count) {
+				printf('Posted by %s on %s  •  %s',
+					$author, $permalink, $comment_count);
+			} else {
+				printf('Posted by %s on %s', $author, $permalink);
+			}
+		} else {
+			if ($show_comment_count) {
+				printf('%s  •  %s', $permalink, $comment_count);
+			} else {
+				echo $permalink;
+			}
+		}
+
+		echo '</div>';
+	}
+
+	// }}}
+
+	// part display methods
+	// {{{ protected function displayPermalink()
+
+	/**
+	 * Displays the date permalink for a weblog post
+	 *
+	 * @param BlorgPost $post
+	 */
+	protected function displayPermalink(BlorgPost $post)
+	{
+		if ($this->getMode('permalink') > SiteView::MODE_NONE) {
+			$link = $this->getLink('permalink');
+			if ($link === false) {
+				$permalink_tag = new SwatHtmlTag('span');
+			} else {
+				$permalink_tag = new SwatHtmlTag('a');
+				if ($link === true) {
+					$permalink_tag->href = $this->getRelativeUri($post);
+				} else {
+					$permalink_tag->href = $link;
+				}
+			}
+			$permalink_tag->class = 'permalink';
+			$permalink_tag->open();
+
+			// display machine-readable date in UTC
+			$abbr_tag = new SwatHtmlTag('abbr');
+			$abbr_tag->class = 'published';
+			$abbr_tag->title = $post->publish_date->getISO8601();
+
+			// display human-readable date in local time
+			$date = clone $post->publish_date;
+			$date->convertTZ($this->app->default_time_zone);
+			$abbr_tag->setContent($date->formatLikeIntl('d MMMM yyyy'));
+			$abbr_tag->display();
+
+			$permalink_tag->close();
+		}
+	}
+
+	// }}}
+}
+
+?>
