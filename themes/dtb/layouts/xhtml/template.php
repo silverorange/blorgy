fbb0080c2472754baaee7de65efeb037d2494c5c
Nick Burka <nick@silverorange.com>
2012-03-07 00:47:55 +0000

    merge live
    svn commit r69759

--- themes/dtb/layouts/xhtml/template.php
+++ themes/dtb/layouts/xhtml/template.php
@@ -1,3 +1,9 @@
+<?php
+if (!isset($_GET['source']) || $_GET['source'] == '') {
+	include dirname(__FILE__).'/splash.html';
+	exit;
+}
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
