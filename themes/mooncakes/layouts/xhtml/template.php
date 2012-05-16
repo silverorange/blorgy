2433782e204099f71dd3568826a970a06028287d
Keith Burgoyne <keith@silverorange.com>
2012-05-16 14:34:01 +0000

    merge to live
    svn commit r72024

--- themes/mooncakes/layouts/xhtml/template.php
+++ themes/mooncakes/layouts/xhtml/template.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
-<html>
+<!--[if lt IE 7 ]> <html class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie8"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html> <!--<![endif]-->
 
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
