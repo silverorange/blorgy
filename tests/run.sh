1aa77900a3cb0dcf92aa5e0c30b753595639ad56
Michael Gauthier <mike@silverorange.com>
2015-02-10 13:06:42 -0400

    Update functional test bootstrap script.
     * better include path generation
     * specify error reporting

--- tests/run.sh
+++ tests/run.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
+
+working_dir=$( basename "$current_dir" )
+site=$( basename "$( dirname "$current_dir" )" )
+
+include_path="/so/packages/swat/"$working_dir\
+":/so/packages/turing/"$working_dir\
+":/so/packages/site/"$working_dir\
+":/so/sites/"$site"/"$working_dir"/pear/lib"
+
 if [ "$1" == "" ]; then
 	echo "phpunit --log-junit log.xml --verbose TestSuite"
 	/usr/bin/phpunit \
-		--include-path '/so/packages/hot-date/trunk:/so/packages/swat/trunk' \
+		--include-path $include_path \
 		--log-junit log.xml \
 		--verbose \
+		-d error_reporting=34815 \
 		TestSuite
 else
 	file="$1"
 	class="`basename "$file" .php`"
-	echo "phpunit --verbose $class $file"
 	/usr/bin/phpunit \
-		--include-path '/so/packages/hot-date/trunk:/so/packages/swat/trunk' \
+		--include-path $include_path \
 		--verbose \
+		-d error_reporting=34815 \
 		$class "$file"
 fi
